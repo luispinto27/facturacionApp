e3e1fe3390c4dcddd518021bff8a9f9ea5891b3f
alain512 <alain.quintero@outlook.com>
2022-11-22 02:28:17 -0500

    Desarrollo rama Alain

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { FacturaComponent } from './backend/factura/factura.component';
 import { ProductosComponent } from './backend/productos/productos.component';
-import { SetClientesComponent } from './backend/set-clientes/set-clientes.component';
+import { CategoriasComponent } from './backend/categorias/categorias.component';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'set-clientes', component: SetClientesComponent },
+  { path: 'categorias', component: CategoriasComponent },
   { path: 'productos', component: ProductosComponent },
+  { path: 'factura', component: FacturaComponent },
   {
     path: '',
     redirectTo: 'home',
--- src/app/backend/categorias/categorias.component.ts
+++ src/app/backend/categorias/categorias.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,  OnInit } from '@angular/core';
+import { MenuController, ToastController } from '@ionic/angular';
+import { InfoCategoriaDTO } from '../../models/models/models.module';
+import { ServiceService } from '../../services/service.service';
+import { NgForm } from '@angular/forms';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-categorias',
@@ -7,8 +12,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriasComponent implements OnInit {
 
-  constructor() { }
+  listCategoria: InfoCategoriaDTO[] = [];
+  infoCategoria: InfoCategoriaDTO = new InfoCategoriaDTO();
+  
+  constructor(public menuController: MenuController,
+    private service: ServiceService,
+    private toastController: ToastController) { }
 
   ngOnInit() {}
+
+  guardarCategoria(formulario: NgForm){
+    // this.service.crearCategoria(this.infoCategoria);
+    this.listCategoria.push({
+      categoriaId: this.infoCategoria.categoriaId,
+      categoriaCodigo: this.infoCategoria.categoriaCodigo,
+      categoriaDescripcion: this.infoCategoria.categoriaDescripcion,
+      categoriaEstado: this.infoCategoria.categoriaEstado,
+      categoriaFechaCreacion: this.infoCategoria.categoriaFechaCreacion,
+      categoriaFechaModificacion: this.infoCategoria.categoriaFechaModificacion
+    });
+    this.limpiarFiltros();
+    this.mostrarNotificaciones('Categoria Guardado Correctamente');
+  }
+
+  openMenu() {
+    this.menuController.toggle('principal');
+  }
+  
+  seleccionaProducto(datos: InfoCategoriaDTO) {
+    this.infoCategoria.categoriaId = datos.categoriaId;
+    this.infoCategoria.categoriaCodigo = datos.categoriaCodigo;
+    this.infoCategoria.categoriaDescripcion = datos.categoriaDescripcion;
+    this.infoCategoria.categoriaEstado = datos.categoriaEstado;
+    this.infoCategoria.categoriaFechaCreacion = datos.categoriaFechaCreacion;
+    this.infoCategoria.categoriaFechaModificacion = datos.categoriaFechaModificacion;
+  }
+
+  async limpiarFiltros(){
+    this.infoCategoria.categoriaId = '';
+    this.infoCategoria.categoriaCodigo = '';
+    this.infoCategoria.categoriaDescripcion = '';
+    this.infoCategoria.categoriaEstado = true;
+    this.infoCategoria.categoriaFechaCreacion = new Date();
+    this.infoCategoria.categoriaFechaModificacion = new Date();
+  }
+
+  async mostrarNotificaciones(message: string){
+    const toast = await this.toastController.create({
+        message: message,
+        duration: 1500,
+        position: 'top'
+    });
+    await toast.present();
+  }
 
 }
