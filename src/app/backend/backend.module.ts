8a7c1f19711d5e06a82debc3760d2490e402802a
Esteban9320 <104812804+Esteban9320@users.noreply.github.com>
2022-11-22 21:34:33 -0500

    component

--- src/app/backend/backend.module.ts
+++ src/app/backend/backend.module.ts
@@ -3,13 +3,22 @@ import { CommonModule } from '@angular/common';
 import { SetClientesComponent } from './set-clientes/set-clientes.component';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
+<<<<<<< Updated upstream
 import { VendedorComponent } from './vendedor/vendedor.component';
+=======
+import { ProductosComponent } from './productos/productos.component';
+
+>>>>>>> Stashed changes
 
 
 @NgModule({
   declarations: [
+<<<<<<< Updated upstream
     SetClientesComponent,
     VendedorComponent
+=======
+    SetClientesComponent, ProductosComponent
+>>>>>>> Stashed changes
   ],
   imports: [
     CommonModule,
